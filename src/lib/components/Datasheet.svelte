8220533ac4d5ef2b9a448a20896925eda3f1ad4c
whohensee <106775295+whohensee@users.noreply.github.com>
2026-04-07 13:15:44 -0700

    Test terminal datasheet

---
+++ src/lib/components/Datasheet.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<!-- The goal of this component is just to test out the
+ black and green terminal theme -->
+
+<div
+	class="flex w-xl flex-col items-center bg-black pt-5 pb-5 text-center font-terminal text-lg text-green-700"
+>
+	<p>NAME: Notorious Criminal</p>
+	<p>CRIME: Heinous Villainy and Evil Actions</p>
+	<p>BIO: He grew up bad, real bad. Better look out for this fella</p>
+	<p>LOCATION: The Underworld</p>
+</div>
--- src/routes/datasheet/+page.svelte
+++ src/routes/datasheet/+page.svelte
@@ -1,1 +1,7 @@
+<script>
+	import Datasheet from '$lib/components/Datasheet.svelte';
+</script>
+
 <h1>The datasheet testing page</h1>
+
+<Datasheet />
