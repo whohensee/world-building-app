8e0fee45b19d3bec76bb62ae7cae0715f5647b98
whohensee <106775295+whohensee@users.noreply.github.com>
2026-04-08 12:40:22 -0700

    Rework generating entities.

--- src/lib/data/basicEntities.ts
+++ src/lib/data/basicEntities.ts
@@ -1,16 +1,46 @@
-const rogues = [
-	{
-		name: 'Evil Jim',
+// const entities = [
+// 	{
+// 		name: 'Evil Jim',
+// 		location: 'The Ninth Circle of Hell',
+// 		faction: 'The Voidspawn',
+// 		description: 'Big n mean and bad.'
+// 	},
+// 	{
+// 		name: 'Goomba',
+// 		location: 'Mushroom Kingdom',
+// 		faction: 'Minions of Bowser',
+// 		description: 'round with lil feet. bouncy'
+// 	},
+// 	{
+// 		name: 'Goomba2',
+// 		location: 'Mushroom Kingdom2',
+// 		faction: 'Minions of Bowser2',
+// 		description: 'round with lil feet. bouncy2'
+// 	}
+// ];
+
+interface thing {
+	name: string;
+	location: string;
+	faction: string;
+	description: string;
+}
+
+const entities: thing[] = [];
+
+for (let i = 0; i < 5; i++) {
+	entities.push({
+		name: `Evil Jim${i}`,
 		location: 'The Ninth Circle of Hell',
 		faction: 'The Voidspawn',
 		description: 'Big n mean and bad.'
-	},
-	{
-		name: 'Goomba',
+	});
+	entities.push({
+		name: `Goomba${i}`,
 		location: 'Mushroom Kingdom',
 		faction: 'Minions of Bowser',
 		description: 'round with lil feet. bouncy'
-	}
-];
+	});
+}
 
-export { rogues };
+export { entities };
--- src/routes/entities/+page.svelte
+++ src/routes/entities/+page.svelte
@@ -1,31 +1,11 @@
 <script lang="ts">
 	import EntityDisplay from '$lib/components/EntityDisplay.svelte';
-
-	const rogues = [
-		{
-			name: 'Evil Jim',
-			location: 'The Ninth Circle of Hell',
-			faction: 'The Voidspawn',
-			description: 'Big n mean and bad.'
-		},
-		{
-			name: 'Goomba',
-			location: 'Mushroom Kingdom',
-			faction: 'Minions of Bowser',
-			description: 'round with lil feet. bouncy'
-		},
-		{
-			name: 'Goomba2',
-			location: 'Mushroom Kingdom2',
-			faction: 'Minions of Bowser2',
-			description: 'round with lil feet. bouncy2'
-		}
-	];
+	import { entities } from '$lib/data/basicEntities';
 
 	let current_index = $state(0);
 	let total_shown = $state(0);
 	function next_index() {
-		if (current_index == rogues.length - 1) {
+		if (current_index == entities.length - 1) {
 			current_index = 0;
 		} else {
 			current_index += 1;
@@ -37,17 +17,21 @@
 <div class="flex flex-col items-center">
 	<h1>The entity browser page</h1>
 
-	<button class="border border-black p-1" onclick={next_index}>Next</button>
-	<!-- This little "trick" keeps svelte transitions from borking if
-     the button is spammed N-1 times or less, where N is the number of total
-     conditionals here. -->
-	{#if total_shown % 4 == 0}
-		<EntityDisplay entity={rogues[current_index]} />
-	{:else if total_shown % 4 == 1}
-		<EntityDisplay entity={rogues[current_index]} />
-	{:else if total_shown % 4 == 2}
-		<EntityDisplay entity={rogues[current_index]} />
-	{:else if total_shown % 4 == 3}
-		<EntityDisplay entity={rogues[current_index]} />
-	{/if}
+	<div
+		class="flex h-100 w-screen flex-col items-center border-t-16 border-b-16 border-taupe-600 bg-black font-terminal text-2xl text-green-700"
+	>
+		<button class="border border-black p-1" onclick={next_index}>Next</button>
+		<!-- This little "trick" keeps svelte transitions from borking if
+		 the button is spammed N-1 times or less, where N is the number of total
+		 conditionals here. -->
+		{#if total_shown % 4 == 0}
+			<EntityDisplay entity={entities[current_index]} />
+		{:else if total_shown % 4 == 1}
+			<EntityDisplay entity={entities[current_index]} />
+		{:else if total_shown % 4 == 2}
+			<EntityDisplay entity={entities[current_index]} />
+		{:else if total_shown % 4 == 3}
+			<EntityDisplay entity={entities[current_index]} />
+		{/if}
+	</div>
 </div>
