c869e3b2ca53690e71224d9d3539db465cc1a107
whohensee <106775295+whohensee@users.noreply.github.com>
2026-04-08 13:18:19 -0700

    Add some data that rougly matches schema

--- src/lib/data/basicEntities.ts
+++ src/lib/data/basicEntities.ts
@@ -26,6 +26,17 @@ interface thing {
 	description: string;
 }
 
+type category = string;
+type ID = number;
+
+interface entity {
+	ID: ID;
+	category: category;
+	relatedTo: ID[];
+	name: string;
+	description: string;
+}
+
 const entities: thing[] = [];
 
 for (let i = 0; i < 5; i++) {
@@ -43,4 +54,35 @@ for (let i = 0; i < 5; i++) {
 	});
 }
 
-export { entities };
+const officialEntities: entity[] = [
+	{
+		ID: 0,
+		category: 'City',
+		relatedTo: [1],
+		name: 'Hive City Karilex',
+		description: 'A doomed hive-city, one of few stranded on a heretical world.'
+	},
+	{
+		ID: 1,
+		category: 'Gang',
+		relatedTo: [0],
+		name: 'The Rust Coats',
+		description: 'Disgraced ex-military gang, known for their vicious shows of force.'
+	},
+	{
+		ID: 2,
+		category: 'Gang Member',
+		relatedTo: [1, 3],
+		name: 'Gangster 1',
+		description: 'He is very violent, and not to be trifled with'
+	},
+	{
+		ID: 3,
+		category: 'Gang Member',
+		relatedTo: [1, 2],
+		name: 'Gangster 2',
+		description: 'Known for thinking of clever plans that rely on brains over brawn.'
+	}
+];
+
+export { entities, officialEntities };
